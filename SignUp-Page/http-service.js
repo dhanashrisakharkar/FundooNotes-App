c8b7b16aa0b1c23097e6111c0c6fd67ca0c0a4e7
Dhanashri Sakharkar <dhanashrisakharkar333@gmail.com>
2021-01-30 00:04:52 +0530

    [Dhanashri] Refator. Reseting the sign page with making api call and removed unused data from page

--- SignUp-Page/http-service.js
+++ SignUp-Page/http-service.js
@@ -1,87 +1,15 @@
-
 const save = (event) => {
     event.preventDefault();
     event.stopPropagation();
     try {
-        createOrUpdateEmployeePayroll();
-        // let userData = createUserDetail();
-        // saveData()
-        // window.addEventListener('DOMContentLoaded', (event) => {
-        //     const fname = document.querySelector('#fname');
-        //     const lname = document.querySelector('#lname');
-        //     const email = document.querySelector('#email');
-        //     const pwd = document.querySelector('#pwd');
-        //     // const rpwd = document.querySelector('#rpwd');
-            
-        //     // console.log(fname,lname,email,pwd,rpwd);
-        //     // let userDataList = JSON.parse(localStorage.getItem("UserDetail"));
-        //     const postElem = document.querySelector('#post_services');
-        //     const postURL = "http://fundoonotes.incubation.bridgelabz.com/api/user/userSignUp";
-        //    const  userData =
-        //     {
-                
-        //         "first name": fname,
-        //         "last name": lname,
-        //         "email": email ,
-        //         "password":pwd,
-        //         "card id" : "_id",
-                
-        //     };
-        //     makeServiceCall("POST", postURL, true, userData)
-        //         .then(responseText => postElem.textContent = "User added : " + responseText)
-        //         .catch(error => postElem.textContent = "POST Error Status: " + JSON.stringify(error));
-            // })
-            return;
-        
-      
+        createAddReset();
+        return;
     } catch (e) {
         return;
     }
 }
 
-// const createUserDetail = () => {
-//     let userData = new SignUpData();
-    
-//     const fname = document.querySelector('#fname');
-//     const lname = document.querySelector('#lname');
-//     const email = document.querySelector('#email');
-//     const pwd = document.querySelector('#pwd');
-
-//     return userData;
-// }
-//   window.addEventListener('DOMContentLoaded', (event) => {
-//   })
-// function saveData()
-// {
-//     window.addEventListener('DOMContentLoaded', (event) => {
-//     const fname = document.querySelector('#fname');
-//     const lname = document.querySelector('#lname');
-//     const email = document.querySelector('#email');
-//     const pwd = document.querySelector('#pwd');
-//     // const rpwd = document.querySelector('#rpwd');
-    
-//     // console.log(fname,lname,email,pwd,rpwd);
-//     // let userDataList = JSON.parse(localStorage.getItem("UserDetail"));
-//     const postElem = document.querySelector('#post_services');
-//     const postURL = "http://fundoonotes.incubation.bridgelabz.com/api/user/userSignUp";
-//     let userData =
-//     {
-        
-//         "first name": fname,
-//         "last name": lname,
-//         "email": email ,
-//         "password": pwd,
-//         "card id" : "-id",
-        
-//     };
-//     makeServiceCall("POST", postURL, true, userData)
-//         .then(responseText => postElem.textContent = "User added : " + responseText)
-//         .catch(error => postElem.textContent = "POST Error Status: " + JSON.stringify(error));
-//     })
-    
-// }
-
-function makeServiceCall(methodType, url, async = true, data ) {
+function makeServiceCall(methodType, url, async = true, data) {
     return new Promise(function (resolve, reject) {
         let xhr = new XMLHttpRequest();
         xhr.onload = function () {
@@ -115,52 +43,42 @@ function makeServiceCall(methodType, url, async = true, data ) {
     });
 }
 
-const createOrUpdateEmployeePayroll = () => {
-    // window.addEventListener('DOMContentLoaded', (event) => {
+const createAddReset = () => {
     const fname = document.querySelector('#fname').value;
-            const lname = document.querySelector('#lname').value;
-            const email = document.querySelector('#email').value;
-            const pwd = document.querySelector('#pwd').value;
-            // var formData = new FormData(document.querySelector('#formElem'))
-            const formData =
-            {
-                
-                firstName: fname,
-                "lastName": lname,
-                "email": email ,
-                "password":pwd,
-                "cartId" : "",
-                "service": "advance"
-                
-            };
-            console.log(formData);
+    const lname = document.querySelector('#lname').value;
+    const email = document.querySelector('#email').value;
+    const pwd = document.querySelector('#pwd').value;
+    const formData =
+    {
+
+        firstName: fname,
+        "lastName": lname,
+        "email": email,
+        "password": pwd,
+        "cartId": "",
+        "service": "advance"
+
+    };
+    console.log(formData);
     let postURL = "http://fundoonotes.incubation.bridgelabz.com/api/user/userSignUp";
     let methodCall = "POST";
-    // if(isUpdate) {
-    //    postURL = postURL + userData.id.toString();
-    //    methodCall = "PUT";
-    // }
     makeServiceCall(methodCall, postURL, true, formData)
-          .then(responseText => {
-             resetForm();
-            //  window.location.replace(site_properties.home_page);
-          })
-          .catch( error => {
-             throw error;
-          });
-        // })
- }
- const resetForm = () => {
+        .then(responseText => {
+            resetForm();
+        })
+        .catch(error => {
+            throw error;
+        });
+}
+const resetForm = () => {
     setValue('#fname', '');
     setValue('#lname', '');
     setValue('#email', '');
     setValue('#pwd', '');
     setValue('#rpwd', '');
-    // setValue('#fname', '');
-    
- }
+}
 
- const setValue = (id, value) => {
+const setValue = (id, value) => {
     const element = document.querySelector(id);
     element.value = value;
- }
+}
