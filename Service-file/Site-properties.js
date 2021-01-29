405099cc0683b3787d8fb649f8fb691ad46046d3
Dhanashri Sakharkar <dhanashrisakharkar333@gmail.com>
2021-01-30 00:33:41 +0530

    [Dhanashri] Add. Path for pages and url for making api call

---
+++ Service-file/Site-properties.js
@@ -0,0 +1,5 @@
+let site_properties = {
+    login_page: "../Login-Page/Login-Page.html",
+    signup_page: "../SignUp-Page/SignUp-Page.html",
+    server_url: "http://fundoonotes.incubation.bridgelabz.com/api/user/userSignUp"
+}
